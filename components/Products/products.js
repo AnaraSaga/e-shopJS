6272964b314f760c46b5476fae3f66db9eb1aa69
Anara Saga <anarasaga@gmail.com>
2023-12-05 21:57:50 +0600

    images added, products.js started

---
+++ components/Products/products.js
@@ -0,0 +1,77 @@
+//import { CATALOG } from './constants/catalog';
+const CATALOG = [
+    {
+        id:"pr01",
+        productName: "Lira Earings",
+        img: "img/pictures/01.png",
+        price: 25.00,
+        description:"",
+    },
+    {
+        id:"pr02",
+        productName: "Hal Earings",
+        img: "img/pictures/02.png",
+        price: 25.00,
+        description:"",
+    },
+    {
+        id:"pr03",
+        productName: "Hair Pin Set of 4",
+        img: "img/pictures/03.png",
+        price: 40.00,
+        description:"",
+    },
+    {
+        id:"pr04",
+        productName: "Kaede Hair Pin Set of 3",
+        img: "img/pictures/04.png",
+        price: 30.00,
+        description:"",
+    },
+    {
+        id:"pr05",
+        productName: "Plaine Necklace",
+        img: "img/pictures/05.png",
+        price: 19.00,
+        description:"",
+    },
+    {
+        id:"pr06",
+        productName: "Yuki Hair Pin Set of 3",
+        img: "img/pictures/06.png",
+        price: 29.00,
+        description:"",
+    }
+];
+
+class Products{
+    
+    //метод отоброжает данные на страничке в виде HTML
+    render(){
+        let htmlCatalog = "";
+
+        //переменная доступна в константах        
+        //+деструктуризация 
+        CATALOG.forEach(({ id, productName, img, price, description})=>{
+            htmlCatalog += 
+            `<li>
+                <img src="${img}"/>
+                <span>${productName}</span>
+                <span>${price}</span>
+                <button>Add to Cart</button>
+            </li>`
+             
+        });
+
+        const html = 
+        `<ul>
+            ${htmlCatalog}
+        </ul>`
+        
+        const ROOT_PRODUCTS = document.getElementById("products");
+        ROOT_PRODUCTS.innerHTML = html;
+    }
+}
+
+const productsPage = new Products();
+productsPage.render();
---
+++ constants/catalog.js
@@ -0,0 +1,44 @@
+// const CATALOG = [
+//     {
+//         id:"1",
+//         productName: "Lira Earings",
+//         img: "img/pictures/01.png",
+//         price: 25.00,
+//         description:"",
+//     },
+//     {
+//         id:"2",
+//         productName: "Hal Earings",
+//         img: "img/pictures/02.png",
+//         price: 25.00,
+//         description:"",
+//     },
+//     {
+//         id:"3",
+//         productName: "Hair Pin Set of 4",
+//         img: "img/pictures/03.png",
+//         price: 40.00,
+//         description:"",
+//     },
+//     {
+//         id:"4",
+//         productName: "Kaede Hair Pin Set of 3",
+//         img: "img/pictures/04.png",
+//         price: 30.00,
+//         description:"",
+//     },
+//     {
+//         id:"5",
+//         productName: "Plaine Necklace",
+//         img: "img/pictures/05.png",
+//         price: 19.00,
+//         description:"",
+//     },
+//     {
+//         id:"6",
+//         productName: "Yuki Hair Pin Set of 3",
+//         img: "img/pictures/06.png",
+//         price: 29.00,
+//         description:"",
+//     }
+// ];
